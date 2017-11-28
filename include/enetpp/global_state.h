8a978ae6aaf1ad88ac837f912ee059ac98aaf5a8
liuwei3 <liuwei3@xiaomi.com>
2017-11-28 13:13:27 +0800

    add file

---
+++ include/enetpp/global_state.h
@@ -0,0 +1,50 @@
+#ifndef ENETPP_GLOBAL_STATE_H_
+#define ENETPP_GLOBAL_STATE_H_
+
+#include <assert.h>
+#include "enet/enet.h"
+
+
+
+namespace enetpp {
+	template<typename T, typename... Args>
+	std::unique_ptr<T> make_unique(Args&&... args) {
+		return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+	};
+
+	//ugh
+	class global_state {
+	private:
+		bool _is_initialized;
+
+	public:
+		static global_state& get() {
+			static global_state g;
+			return g;
+		}
+
+	public:
+		bool is_initialized() const {
+			return _is_initialized;
+		}
+
+		void initialize() {
+			assert(!_is_initialized);
+			enet_initialize();
+			_is_initialized = true;
+		}
+
+		void deinitialize() {
+			enet_deinitialize();
+			_is_initialized = false;
+		}
+
+	private:
+		global_state()
+			: _is_initialized(false) {
+		}
+	};
+
+}
+
+#endif
